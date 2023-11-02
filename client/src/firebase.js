fd51f00130f1294a74522dc718a1c7a090e61230
Dhinesh4668 <dhinesh4668@gmail.com>
2023-11-02 13:24:20 +0530

    firebase config

---
+++ client/src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8wdO9_zSjhKPTsFMD8QNkwTU016j9gP4",
+  authDomain: "intern-project-a325a.firebaseapp.com",
+  projectId: "intern-project-a325a",
+  storageBucket: "intern-project-a325a.appspot.com",
+  messagingSenderId: "324669972881",
+  appId: "1:324669972881:web:abf3746e73b8186e47fa30",
+  measurementId: "G-F6LZMKFKFV"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
