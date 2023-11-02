0493fbe5dd0f77e4fdcd68405128e6d311866e79
Dhinesh4668 <dhinesh4668@gmail.com>
2023-11-02 11:57:57 +0530

    basic RESTApi

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 const mysql = require('mysql')
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
 
-
+// database connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'task-tracker'
+})
+// database error handling
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database: ' + err.message);
+    return;
+    }console.log('Connected to the database');
+  });
 
 // simple rest api 
 app.get('/',(req,res)=>{
